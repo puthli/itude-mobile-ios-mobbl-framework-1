2b6d1eb729d2b6251f07795ad2da81ea49cfae28
Pieter Kuijpers <pieter@pi-q.nl>
2012-03-20 14:41:55 +0100

    Added unit test target.

---
+++ itude-mobile-iphone-coreLogicTests/itude_mobile_iphone_coreLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  itude_mobile_iphone_coreLogicTests.m
+//  itude-mobile-iphone-coreLogicTests
+//
+//  Created by Pieter Kuijpers on 20-03-12.
+//  Copyright (c) 2012 Itude Mobile BV. All rights reserved.
+//
+
+#import "itude_mobile_iphone_coreLogicTests.h"
+
+@implementation itude_mobile_iphone_coreLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in itude-mobile-iphone-coreLogicTests");
+}
+
+@end
