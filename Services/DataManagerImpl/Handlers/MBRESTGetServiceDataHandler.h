ef600aaf28d1ead38841a505a06d7f77e48eb8f9
Pieter Kuijpers <pieter@pi-q.nl>
2012-06-05 20:52:40 +0200

    Added getRequestUrlForDocument to public API to override in subclasses.

--- Services/DataManagerImpl/Handlers/MBRESTGetServiceDataHandler.h
+++ Services/DataManagerImpl/Handlers/MBRESTGetServiceDataHandler.h
@@ -20,6 +20,9 @@
 - (NSURLConnection *)createConnectionAndStartLoadingWithRequest:(NSURLRequest *)request delegate:(MBRequestDelegate *)delegate;
 - (MBDocument *)createDocumentWithName:(NSString *)documentName forData:(NSData *)data;
 
+- (NSString *)getRequestUrlForDocument:(NSString *)documentName WithArguments:(MBDocument *)arguments;
+
+
 - (NSMutableURLRequest *) setupHTTPRequest:(NSMutableURLRequest *)request;
 
 @end
