030cebb6b4cfd04afc59b21a99adda47b0809d42
Frank van Eenbergen <f.vaneenbergen@itude.com>
2013-09-13 11:26:26 +0200

    Added features to set a resizable background image on the UITextField.

---
+++ ios-mobbl/Util/UITextField+ResizableBackgroundImage.h
@@ -0,0 +1,13 @@
+//
+//  UITextField+ResizableBackgroundImage.h
+//  Binck
+//
+//  Created by Frank van Eenbergen on 9/13/13.
+//  Copyright (c) 2013 Itude Mobile BV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (ResizableBackgroundImage)
+- (void)setResizableBackgroundImage:(UIImage *)image;
+@end
