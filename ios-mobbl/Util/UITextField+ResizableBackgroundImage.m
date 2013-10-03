298b598f4f6e7e9bffbf8c1fac6d9d35ee2f5f68
w.elsinga@itude.com <w.elsinga@itude.com>
2013-10-03 10:57:23 +0200

    Added header information to all .h and .m files

--- ios-mobbl/Util/UITextField+ResizableBackgroundImage.m
+++ ios-mobbl/Util/UITextField+ResizableBackgroundImage.m
@@ -1,10 +1,18 @@
-//
-//  UITextField+ResizableBackgroundImage.m
-//  Binck
-//
-//  Created by Frank van Eenbergen on 9/13/13.
-//  Copyright (c) 2013 Itude Mobile BV. All rights reserved.
-//
+/*
+ * (C) Copyright ItudeMobile.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "UITextField+ResizableBackgroundImage.h"
 
